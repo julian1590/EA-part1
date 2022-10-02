7e5193e515fbf2cb0ea6721d9849cd24ff7ee84a
Julian1590 <julian159@live.nl>
2022-10-02 11:11:29 +0200

    FINAL COMMIT (I HOPE)

--- run_best.py
+++ run_best.py
@@ -8,34 +8,46 @@ from demo_controller import player_controller
 import os
 import numpy as np
 
+headless = True
+if headless:
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
 
-# runs simulation
-def simulation(env, x):
-    f, p, e, t = env.play(pcont=x)
-    return f
-
-
-# evaluation
-def evaluate(x, env):
-    return np.array(list(map(lambda y: simulation(env, y), x)))
+def get_best(en, run):
+    enemy_dir = f"Enemy{en}"
+    experiment_path = ""
+    for dir in os.listdir(enemy_dir):
+        if dir.endswith(f"run-{run}"):
+            experiment_path = dir
+    best_run_path = os.path.join(enemy_dir, experiment_path)
+    return best_run_path
 
 
 def test():
-    best_run_path = os.path.join("Cauchy-to-gauss",
-                                 "enemy_[1]_experiment_pop-110_tourn_size-2_selec-default_cross-k_point_mut-combined_mutProb-0.5_k-10_run-2")
+    experiment_name = "Collecting-boxplots-data"
+    enemies = {}
+    for en in range(1, 4):
+        enemies[en] = {}
+        for run in range(10):
+            best_run_path = get_best(en, run)
+            env = Environment(experiment_name=experiment_name,
+                              playermode="ai",
+                              enemies = [en],
+                              player_controller=player_controller(10),
+                              speed="fastest",
+                              logs="off",
+                              enemymode="static",
+                              level = 2)
+            bsol = np.loadtxt(best_run_path + '/best.txt')
+            individual_gains = []
+            for i in range(5):
+                print(f"Running enemy {en} run {run} test {i}")
+                f, p, e, t = env.play(bsol)
+                individual_gain = p - e
+                individual_gains.append(individual_gain)
+            enemies[en][run] = individual_gains
+    with open("boxplot_results.txt", "w") as f:
+        json.dump(enemies, f)
 
-    env = Environment(experiment_name=best_run_path,
-                      enemies=[1],
-                      playermode="ai",
-                      player_controller=player_controller(10),
-                      enemymode="static",
-                      level=2,
-                      speed="normal")
-    bsol = np.loadtxt(best_run_path + '/best.txt')
-    print('\n RUNNING SAVED BEST SOLUTION \n')
-    env.update_parameter('speed', 'normal')
-    evaluate([bsol], env)
-    sys.exit(0)
 
 if __name__ == "__main__":
     test()
